56f3d8ebf211e316d1baa7f08bcfcde784257ed3
s.golovachev <s.golovachev@acase.ru>
2018-07-11 17:47:53 +0300

    add history

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const ytdl = require('ytdl-core');
 
 window.onload = function() {
+    window.onpopstate = processUrl;
     document.getElementById('url').addEventListener('change', validateUrl);
+    processUrl();
 };
+
+function processUrl() {
+    var url = decodeURIComponent(window.location.search.substr(5));
+    if (url != null && url != '') {
+        drawDisabledVideos();
+        drawDisabledAudios();
+        document.getElementById('url').value = url;
+        fetchInfo(url);
+    }
+}
 
 function validateUrl() {
     var url = document.getElementById('url').value;
     if (url != null) {
+        history.pushState(null, null, window.location.href.split('?', 2)[0] + '?url=' + encodeURIComponent(url));
         fetchInfo(url);
     }
 }
@@ -27,9 +40,7 @@ function fetchYoutubeUrl(info, options) {
     // var link = document.getElementById('link');
     // link.setAttribute('href', format.url) ;
     // link.innerText = format.url;
-    var thumbs = info.player_response.videoDetails.thumbnail.thumbnails;
-    document.getElementById('previewImg').setAttribute('src', thumbs[thumbs.length - 1].url);
-    drawButtons(info.title, info.formats);
+    drawButtons(info, info.formats);
 }
 
 function transformUrl(url) {
@@ -43,12 +54,12 @@ function transformUrl(url) {
     return url;
 }
 
-function drawButtons(title, formats) {
-    processVideos(title, formats);
-    processAudios(title, formats);
+function drawButtons(info, formats) {
+    processVideos(info, formats);
+    processAudios(info.title, formats);
 }
 
-function processVideos(title, formats) {
+function processVideos(info, formats) {
     var resolutions = Array.from(new Set(formats.filter(format => format.resolution != null && format.resolution.endsWith('p')).
     map(format => format.resolution))).sort(function(a, b) {return parseInt(b) - parseInt(a)});
     var videos = [];
@@ -78,28 +89,40 @@ function processVideos(title, formats) {
             fulls.unshift(highestVideos[i]);
         }
     }
-    drawVideos(title, videos);
+    drawVideos(info, videos);
 }
 
-function drawVideos(title, videos) {
+function drawVideos(info, videos) {
+    drawDisabledVideos();
     // var videoLabel = document.getElementById('videoLabel');
     // videoLabel.classList.remove('disabled');
-    document.getElementById('previewImg').classList.remove('disabled');
-    var videoButtons = document.getElementsByClassName('videoButton');
-    for (var i in Object.keys(videoButtons)) {
-        videoButtons[i].style.visibility = 'visible';
-        setVideoButtonDisabled(videoButtons[i]);
-        Object.assign(videoButtons[i].style, {'background-image' : "none"});
-    }
+    var thumbs = info.player_response.videoDetails.thumbnail.thumbnails;
+    document.getElementById('previewImg').setAttribute('src', thumbs[thumbs.length - 1].url);
+    document.getElementById('previewImg').style.height = 'auto';
     for (let i = 0; i < videos.length; i++) {
         var button = document.getElementById('p' + videos[i].resolution.substr(0, videos[i].resolution.length - 1));
         button.style.visibility  = 'visible';
         button.classList.remove('disabled');
         button.children[0].setAttribute('href', videos[i].url);
-        button.children[0].setAttribute('download', title);
+        button.children[0].setAttribute('download', info.title);
         if (videos[i].audioBitrate == null) {
             Object.assign(button.style, {'background-image' : "url('./no-sound.png')"});
         }
+    }
+}
+
+function drawDisabledVideos() {
+    var previewImg = document.getElementById('previewImg');
+    previewImg.src = '//:0';
+    previewImg.style.height = 0;
+    if (!previewImg.classList.contains('disabled')) {
+        previewImg.classList.add('disabled')
+    }
+    var videoButtons = document.getElementsByClassName('videoButton');
+    for (var i in Object.keys(videoButtons)) {
+        videoButtons[i].style.visibility = 'visible';
+        setVideoButtonDisabled(videoButtons[i]);
+        Object.assign(videoButtons[i].style, {'background-image' : "none"});
     }
 }
 
@@ -122,18 +145,23 @@ function processAudios(title, formats) {
 }
 
 function drawAudios(title, audios) {
-    // var audioLabel = document.getElementById('audioLabel');
-    // audioLabel.classList.remove('disabled');
-    var audioButtons = document.getElementsByClassName('audioButton');
-    for (var i in Object.keys(audioButtons)) {
-        audioButtons[i].style.visibility = 'visible';
-        setVideoButtonDisabled(audioButtons[i]);
-    }
+    drawDisabledAudios();
     for (let i = 0; i < audios.length; i++) {
         var button = document.getElementById('kb' + audios[i].audioBitrate);
         button.style.visibility  = 'visible';
         button.classList.remove('disabled');
         button.children[0].setAttribute('href', audios[i].url);
         button.children[0].setAttribute('download', title);
+    }
+}
+
+
+function drawDisabledAudios() {
+    // var audioLabel = document.getElementById('audioLabel');
+    // audioLabel.classList.remove('disabled');
+    var audioButtons = document.getElementsByClassName('audioButton');
+    for (var i in Object.keys(audioButtons)) {
+        audioButtons[i].style.visibility = 'visible';
+        setVideoButtonDisabled(audioButtons[i]);
     }
 }
--- lol.js
+++ lol.js
@@ -8770,12 +8770,25 @@ function extend() {
 const ytdl = require('ytdl-core');
 
 window.onload = function() {
+    window.onpopstate = processUrl;
     document.getElementById('url').addEventListener('change', validateUrl);
+    processUrl();
 };
+
+function processUrl() {
+    var url = decodeURIComponent(window.location.search.substr(5));
+    if (url != null && url != '') {
+        drawDisabledVideos();
+        drawDisabledAudios();
+        document.getElementById('url').value = url;
+        fetchInfo(url);
+    }
+}
 
 function validateUrl() {
     var url = document.getElementById('url').value;
     if (url != null) {
+        history.pushState(null, null, window.location.href.split('?', 2)[0] + '?url=' + encodeURIComponent(url));
         fetchInfo(url);
     }
 }
@@ -8796,9 +8809,7 @@ function fetchYoutubeUrl(info, options) {
     // var link = document.getElementById('link');
     // link.setAttribute('href', format.url) ;
     // link.innerText = format.url;
-    var thumbs = info.player_response.videoDetails.thumbnail.thumbnails;
-    document.getElementById('previewImg').setAttribute('src', thumbs[thumbs.length - 1].url);
-    drawButtons(info.title, info.formats);
+    drawButtons(info, info.formats);
 }
 
 function transformUrl(url) {
@@ -8812,12 +8823,12 @@ function transformUrl(url) {
     return url;
 }
 
-function drawButtons(title, formats) {
-    processVideos(title, formats);
-    processAudios(title, formats);
+function drawButtons(info, formats) {
+    processVideos(info, formats);
+    processAudios(info.title, formats);
 }
 
-function processVideos(title, formats) {
+function processVideos(info, formats) {
     var resolutions = Array.from(new Set(formats.filter(format => format.resolution != null && format.resolution.endsWith('p')).
     map(format => format.resolution))).sort(function(a, b) {return parseInt(b) - parseInt(a)});
     var videos = [];
@@ -8847,28 +8858,40 @@ function processVideos(title, formats) {
             fulls.unshift(highestVideos[i]);
         }
     }
-    drawVideos(title, videos);
+    drawVideos(info, videos);
 }
 
-function drawVideos(title, videos) {
+function drawVideos(info, videos) {
+    drawDisabledVideos();
     // var videoLabel = document.getElementById('videoLabel');
     // videoLabel.classList.remove('disabled');
-    document.getElementById('previewImg').classList.remove('disabled');
-    var videoButtons = document.getElementsByClassName('videoButton');
-    for (var i in Object.keys(videoButtons)) {
-        videoButtons[i].style.visibility = 'visible';
-        setVideoButtonDisabled(videoButtons[i]);
-        Object.assign(videoButtons[i].style, {'background-image' : "none"});
-    }
+    var thumbs = info.player_response.videoDetails.thumbnail.thumbnails;
+    document.getElementById('previewImg').setAttribute('src', thumbs[thumbs.length - 1].url);
+    document.getElementById('previewImg').style.height = 'auto';
     for (let i = 0; i < videos.length; i++) {
         var button = document.getElementById('p' + videos[i].resolution.substr(0, videos[i].resolution.length - 1));
         button.style.visibility  = 'visible';
         button.classList.remove('disabled');
         button.children[0].setAttribute('href', videos[i].url);
-        button.children[0].setAttribute('download', title);
+        button.children[0].setAttribute('download', info.title);
         if (videos[i].audioBitrate == null) {
             Object.assign(button.style, {'background-image' : "url('./no-sound.png')"});
         }
+    }
+}
+
+function drawDisabledVideos() {
+    var previewImg = document.getElementById('previewImg');
+    previewImg.src = '//:0';
+    previewImg.style.height = 0;
+    if (!previewImg.classList.contains('disabled')) {
+        previewImg.classList.add('disabled')
+    }
+    var videoButtons = document.getElementsByClassName('videoButton');
+    for (var i in Object.keys(videoButtons)) {
+        videoButtons[i].style.visibility = 'visible';
+        setVideoButtonDisabled(videoButtons[i]);
+        Object.assign(videoButtons[i].style, {'background-image' : "none"});
     }
 }
 
@@ -8891,19 +8914,24 @@ function processAudios(title, formats) {
 }
 
 function drawAudios(title, audios) {
-    // var audioLabel = document.getElementById('audioLabel');
-    // audioLabel.classList.remove('disabled');
-    var audioButtons = document.getElementsByClassName('audioButton');
-    for (var i in Object.keys(audioButtons)) {
-        audioButtons[i].style.visibility = 'visible';
-        setVideoButtonDisabled(audioButtons[i]);
-    }
+    drawDisabledAudios();
     for (let i = 0; i < audios.length; i++) {
         var button = document.getElementById('kb' + audios[i].audioBitrate);
         button.style.visibility  = 'visible';
         button.classList.remove('disabled');
         button.children[0].setAttribute('href', audios[i].url);
         button.children[0].setAttribute('download', title);
+    }
+}
+
+
+function drawDisabledAudios() {
+    // var audioLabel = document.getElementById('audioLabel');
+    // audioLabel.classList.remove('disabled');
+    var audioButtons = document.getElementsByClassName('audioButton');
+    for (var i in Object.keys(audioButtons)) {
+        audioButtons[i].style.visibility = 'visible';
+        setVideoButtonDisabled(audioButtons[i]);
     }
 }
 },{"ytdl-core":57}],47:[function(require,module,exports){
